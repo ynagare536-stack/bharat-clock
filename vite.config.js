dd02b60d3e1193fc2bb0aa63432a3d92d28174b7
Yash Nagare <ynagare536@gmail.com>
2026-08-25 22:51:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base: "/bharat-clock/",
-
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-  },
-});
+  base: '/bharat-clock/',
+})
